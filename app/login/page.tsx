60c8fb519b40aec9aea9661a9dfa27e8e7dcdd74
2000Deba <deep2000seal@gmail.com>
2026-05-17 01:30:06 +0530

    Initial commit

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -237,6 +237,9 @@ export default function LoginPage() {
         const toastId = toast.loading("Signing you in...");
 
         try {
+            await delay(1500);
+            updateToast(toastId, "success", "Login successful 🎉");
+            await delay(1000);
             await signIn(provider, {
                 callbackUrl: "/dashboard",
             });
